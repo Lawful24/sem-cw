1b7e11511c3e32b730de767c6bb29c1be97af135
Lawful <tarkanyilacko@gmail.com>
2022-02-22 15:29:25 +0000

    Created DatabaseSingleton class and imported world.sql

--- src/main/java/com/napier/semCw/App.java
+++ src/main/java/com/napier/semCw/App.java
@@ -1,7 +1,17 @@
 package com.napier.semCw;
 
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        DatabaseSingleton db = new DatabaseSingleton();
+
+        // Connect to database
+        db.connect();
+
+        // Disconnect from database
+        db.disconnect();
     }
 }
