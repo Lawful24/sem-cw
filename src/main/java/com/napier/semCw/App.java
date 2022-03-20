72ac4d1cf7c621efab043c970fed3be87bee55e6
Laszlo Tarkanyi <40493602@live.napier.ac.uk>
2022-03-20 20:26:02 +0000

    Updated connect() with location and delay parameters

--- src/main/java/com/napier/semCw/App.java
+++ src/main/java/com/napier/semCw/App.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-
         DatabaseSingleton db = new DatabaseSingleton();
 
         // Connect to database
-        db.connect();
+        if(args.length < 1) {
+            db.connect("localhost:33060", 30000);
+        } else {
+            db.connect(args[0], Integer.parseInt(args[1]));
+        }
 
         // Disconnect from database
         db.disconnect();
