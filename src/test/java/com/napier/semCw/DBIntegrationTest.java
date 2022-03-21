383e27d86881aaefd56938e748da79962bc03f0b
Laszlo Tarkanyi <40493602@live.napier.ac.uk>
2022-03-21 20:31:15 +0000

    Migrated tests into DBIntegrationTest

--- src/test/java/com/napier/semCw/DBIntegrationTest.java
+++ src/test/java/com/napier/semCw/DBIntegrationTest.java
@@ -3,7 +3,9 @@ package com.napier.semCw;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DBIntegrationTest {
     static DatabaseSingleton db;
@@ -16,7 +18,69 @@ public class DBIntegrationTest {
     }
 
     @Test
-    void testAssertEquals() {
-        assertEquals(5, 5);
+    void getAllCountriesFromDatabaseTestNotNull() {
+        assertNotNull(db.getAllCountriesFromDatabase());
+    }
+
+    @Test
+    void getAllCountriesFromDatabaseTestContainsNull() {
+        List<Country> results = db.getAllCountriesFromDatabase();
+        assertFalse(results.contains(null));
+    }
+
+    @Test
+    void getAllCountriesFromDatabaseTestContainsDefaultObject() {
+        List<Country> results = db.getAllCountriesFromDatabase();
+        for (Country c : results) {
+            assertNotNull(c.code);
+            assertNotNull(c.name);
+            assertNotNull(c.continent);
+            assertNotNull(c.region);
+            assertNotNull(c.language);
+        }
+    }
+
+    @Test
+    void getAllCitiesFromDatabaseTestNotNull() {
+        assertNotNull(db.getAllCitiesFromDatabase());
+    }
+
+    @Test
+    void getAllCitiesFromDatabaseTestContainsNull() {
+        List<City> results = db.getAllCitiesFromDatabase();
+        assertFalse(results.contains(null));
+    }
+
+    @Test
+    void getAllCitiesFromDatabaseTestContainsDefaultObject() {
+        List<City> results = db.getAllCitiesFromDatabase();
+        for (City c : results) {
+            assertNotNull(c.getCountryCode());
+            assertNotNull(c.getName());
+            assertNotNull(c.getDistrictName());
+            assertTrue(c.getId() >= 0);
+            assertTrue(c.getPopulation() >= 0);
+        }
+    }
+
+    @Test
+    void getAllLanguagesFromDatabaseTestNotNull() {
+        assertNotNull(db.getAllLanguagesFromDatabase());
+    }
+
+    @Test
+    void getAllLanguagesFromDatabaseTestContainsNull() {
+        List<Language> results = db.getAllLanguagesFromDatabase();
+        assertFalse(results.contains(null));
+    }
+
+    @Test
+    void getAllLanguagesFromDatabaseTestContainsDefaultObject() {
+        List<Language> results = db.getAllLanguagesFromDatabase();
+        for (Language l : results) {
+            assertNotNull(l.getCountryCode());
+            assertNotNull(l.getLanguage());
+            assertTrue(l.getPercentage() >= 0);
+        }
     }
 }
